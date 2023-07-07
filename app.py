2868b14d8db7c1c133292f4a65f5ad416205b0d9
Himanshu Goyal <hggoyal06@gmail.com>
2023-07-08 00:00:22 +0530

    Update app.py
    UPDATED PDF -  CHARTS ETC

--- app.py
+++ app.py
@@ -1,15 +1,215 @@
-import pandas as pd  # pip install pandas openpyxl
 # import plotly.express as px  # pip install plotly-express
 # import plotly.graph_objects as pxgo
-import streamlit as st  # pip install streamlit
+import streamlit as st
 import datetime
-import io
 import requests, json
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 import base64, calendar, time
 import altair as alt
+
+import matplotlib
+from pylab import title, figure, xlabel, ylabel, xticks, bar, legend, axis, savefig
+from fpdf import FPDF
+from pypdf import PdfMerger
+import cv2
+
+
+
+class FPDF(FPDF):
+    def footer(self):
+        # Go to 1.5 cm from bottom
+        self.set_y(-15)
+        # Select Arial italic 8
+        self.set_font('Arial', 'I', 8)
+        # Print centered page number
+        self.cell(0, 10, "Developed by Himanshu Goyal - Hggoyal06@gmail.com", 0, 0, 'R')
+
+
+def pdf_first_page(city, month,year):
+
+    border = 0
+    pdf = FPDF(orientation = 'P', unit = 'mm', format='A4')
+
+    pdf.add_page()
+    pdf.set_xy(0, 0)
+    pdf.set_font('arial', 'B', 12)
+    pdf.set_margins(10,10,10)
+
+    pdf.image('logo.png', x = 105-25, y = 10, w = 50, h = 25, type = '', link = '')
+
+    pdf.set_font('', '', 10)
+
+    pdf.cell(10, 50, ln = 1, border = border)
+
+
+    text = """This Report is generated from publicly available data by Storm Liquor license."""
+    pdf.cell(10)
+    pdf.cell(0, 5, txt = text, border = border, ln = 1, align = '', fill = False, link = '')
+
+    text = "Visit us at https://stormliquorlicense.com/"
+    pdf.cell(10)
+    pdf.cell(0, 5, txt = text, border = border, ln = 1, align = '', fill = False, link = '')
+
+    pdf.cell(10,10, ln=1, border = border)
+
+    pdf.set_font('', 'B', 12)
+    pdf.cell(10,5,border = border)
+    pdf.cell(0, 5, txt = f'Summary for {city} for month of {month}-{year}', border = border, ln = 1, align = 'L', fill = False, link = '')
+
+    pdf.cell(10,5, ln=1, border = border)
+
+    h = 748
+    w = 916
+
+    neww = 100
+    newh = neww/w*h
+
+    pdf.set_font('', 'U', 10)
+    pdf.cell(10, 5,border = border)
+    pdf.cell(0 , 5, txt = 'Chart 1 : Total Receipts by Type', border = border, ln = 1, align = 'L', fill = False, link = '')
+
+    pdf.cell(10, 5,border = border, ln = 1)
+
+    pdf.cell(10, newh,border = border)
+    pdf.image('output1.png', x = pdf.x, y = pdf.y, w = neww, h = newh, type = '', link = '')
+
+################################################
+
+    pdf.add_page()
+
+    pdf.cell(10)
+
+    h = 1078
+    w = 1089
+    neww = 150
+    newh = neww/w*h
+
+    pdf.cell(0, 10,border = border, ln = 1)
+
+    # pdf.cell(10, 5, border = border)
+    pdf.cell(0 , 5, txt = 'Chart 2 : Top 25 Locations by Total Reciepts', border = border, ln = 1, align = 'L', fill = False, link = '')
+
+
+    pdf.cell(10, 5,border = border, ln = 1)
+
+    pdf.cell(10, newh,border = border)
+    pdf.image('Top25.png', x = pdf.x, y = pdf.y, w = neww, h = newh, type = '', link = '')
+
+    pdf.output('FirstPage.pdf', 'F')
+
+
+def create_plots(df):
+
+    ##########################################
+    ##############Total Reciepts##############
+    ##########################################
+
+    LiquorReceipts = sum(pd.to_numeric(df["Liquor Reciepts"]))
+    WineReciepts   = sum(pd.to_numeric(df["Wine Reciepts"]))
+    BeerReciepts   = sum(pd.to_numeric(df["Beer Reciepts"]))
+    CoverChargeReciepts = sum(pd.to_numeric(df["Cover Charge Reciepts"]))
+    TotalReciepts = sum(pd.to_numeric(df["Total Reciepts"]))
+
+    print("LiquorReceipts:", LiquorReceipts)
+    print("WineReciepts:", WineReciepts)
+    print("BeerReciepts:", BeerReciepts)
+    print("CoverChargeReciepts:", CoverChargeReciepts)
+    print("TotalReciepts:", TotalReciepts)
+
+
+    fig, ax = plt.subplots(figsize =(8, 8))
+
+    ax.bar(["Liquor Receipts","Wine Reciepts","Beer Reciepts","Cover Charge Reciepts","Total Reciepts"], [LiquorReceipts, WineReciepts, BeerReciepts, CoverChargeReciepts, TotalReciepts])
+    ax.invert_xaxis()
+
+    ax.get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
+
+    for i in ax.patches:
+        # plt.text(i.get_x()+ 0.5, i.get_height() + 1000000, "$ {:,.2f}".format(i.get_height()), fontsize = 8, fontweight ='bold', color ='grey', rotation = 90)
+        plt.text(i.get_x()+ 0.5, i.get_height(), "  $ {:,.2f}".format(i.get_height()), fontsize = 8, fontweight ='bold', color ='grey', rotation = 90)
+
+    ax.grid(b = True, color ='grey', linestyle ='-.', linewidth = 0.5, alpha = 0.2)
+
+    plt.yticks(fontsize = 10)
+    plt.xticks(rotation = 90, fontsize = 8)
+    plt.savefig("output1", facecolor='w', bbox_inches="tight", pad_inches=0.3, transparent=True) 
+
+    ##########################################
+    #######Top 25 Locations by Reciepts#######
+    ##########################################
+
+    total_chart = df[["Location Name","Total Reciepts"]].head(25)
+
+    fig, ax = plt.subplots(figsize =(8, 8))
+    fig.set_size_inches(12, 8)
+
+    ax.bar(total_chart["Location Name"], total_chart["Total Reciepts"])
+
+    for i in ax.patches:
+
+        # plt.text( i.get_x() + 0.2, i.get_height() + 10000, "$ {:,.2f}".format(i.get_height()), fontsize = 8, fontweight ='bold', color ='grey', rotation = 90)
+        plt.text( i.get_x() + 0.2, i.get_height(), "  $ {:,.2f}".format(i.get_height()), fontsize = 8, fontweight ='bold', color ='grey', rotation = 90)
+
+    ax.get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
+
+    plt.ylabel('Location Name', fontsize=10)
+    plt.xlabel('Total Reciepts', fontsize=10)
+    plt.title('Top 25 Locations by Total Reciepts')
+
+    plt.yticks(fontsize = 10)
+    plt.xticks(rotation = 90, fontsize = 8)
+    plt.savefig("Top25", facecolor='w', bbox_inches="tight", pad_inches=0.3, transparent=True) 
+
+
+
+# def create_pdf(df, city, month,year):
+#     ########## Create PDF
+
+#     create_plots(df)
+#     pdf_first_page(city, month,year)
+
+#     pp = PdfPages("Data.pdf")
+
+#     n = 50
+#     list_df = [df[i:i+n] for i in range(0,len(df),n)]
+
+#     for df in list_df[0:5]:
+
+#         print("Running")
+
+#         fig, ax = plt.subplots(figsize=(12,12),squeeze=True)
+
+#         ax.axis('tight')
+#         ax.axis('off')
+
+#         the_table = ax.table(cellText = df.values , colLabels = df.columns, loc='center', cellLoc='center')
+#         the_table.auto_set_font_size(False)
+#         the_table.set_fontsize(5)
+#         the_table.auto_set_column_width(col=list(range(len(df.columns))))
+
+#         pp.savefig(fig, bbox_inches='tight')
+
+#     pp.close()
+
+#     merger = PdfMerger()
+#     for pdf in ["FirstPage.pdf", "Data.pdf"]:
+#         merger.append(pdf)
+
+#     merger.write("Final.pdf")
+#     merger.close()
+
+    # with open("foo.pdf", "rb") as pdf_file:
+    #     encoded_string = pdf_file.read()
+    
+    # return encoded_string
+
+
+# df = pd.read_excel("Excel.xlsx")
+# create_pdf(df, "Dallas", "2", "2023")
+# input("----")
 
 ###################################
 
@@ -138,11 +338,11 @@ def fetch_df():
     else:
         df = pd.DataFrame([], columns = ["index","location_name","location_address","location_zip","tabc_permit_number","liquor_receipts","wine_receipts","beer_receipts","cover_charge_receipts","total_receipt"])
 
-    cols = ["index", "Location Name", "Location Address", "Location Zip","TABC Permit Num", "Liquor Receipts", "Wine Reciepts","Beer Reciepts", "Cover Charge Reciepts", "Total Reciepts"]
+    cols = ["index", "Location Name", "Location Address", "Location Zip","TABC Permit Num", "Liquor Reciepts", "Wine Reciepts","Beer Reciepts", "Cover Charge Reciepts", "Total Reciepts"]
     df.columns = cols
 
-
     return df, month, year
+
 
 
 if "unique_cities" not in st.session_state:
@@ -173,31 +373,54 @@ else:
     fetch = st.sidebar.button("Fetch Data", key="Fetch", on_click = fetch_df, disabled = False)
 
 
-def create_pdf(df):
+# @st.cache
+def create_pdf(df, city, month,year):
     ########## Create PDF
+    print("Running Create PDF")
 
-    fig, ax = plt.subplots(figsize=(12,12),squeeze=True)
+    create_plots(df)
+    pdf_first_page(city, month,year)
 
-    ax.axis('tight')
-    ax.axis('off')
+    pp = PdfPages("Data.pdf")
 
-    the_table = ax.table(cellText=df.values , colLabels=df.columns, loc='center', cellLoc='center')
-    the_table.auto_set_font_size(False)
-    the_table.set_fontsize(5)
-    the_table.auto_set_column_width(col=list(range(len(df.columns))))
+    n = 50
 
-    pp = PdfPages("foo.pdf")
+    list_df = [df[i:i+n] for i in range(0,len(df),n)]
 
-    # pp.text(0.05,0.95,txt, transform=fig.transFigure, size=24)
 
-    pp.savefig(fig, bbox_inches='tight')
+    for i,df in enumerate(list_df,1):
+
+        print(f"Page {i}..")
+        fig, ax = plt.subplots(figsize=(12,12),squeeze=True)
+
+        ax.axis('tight')
+        ax.axis('off')
+
+        the_table = ax.table(cellText=df.values , colLabels = df.columns, loc='center', cellLoc='center')
+        the_table.auto_set_font_size(False)
+        the_table.set_fontsize(5)
+        the_table.auto_set_column_width(col=list(range(len(df.columns))))
+
+        pp.savefig(fig, bbox_inches='tight')
+
     pp.close()
 
-    with open("foo.pdf", "rb") as pdf_file:
+    merger = PdfMerger()
+    for pdf in ["FirstPage.pdf", "Data.pdf"]:
+        merger.append(pdf)
+
+    merger.write("Final.pdf")
+    merger.close()
+
+    with open("Final.pdf", "rb") as pdf_file:
         encoded_string = pdf_file.read()
     
     return encoded_string
 
+
+# df = pd.read_excel("Excel.xlsx")
+# create_pdf(df)
+# input("----")
 
 
 def create_charts():
@@ -235,9 +458,11 @@ def create_charts():
     st.write(df.drop(columns = ["index"]))
 
     if len(df.index) > 0:
-        csv = create_pdf(df)
 
-        st.download_button("Export Data", csv, f"{city}-{year}-{month}.pdf", mime='application/octet-stream', key='download-csv', on_click = create_charts)
+        csv = create_pdf(df, city, month, year)
+
+        st.download_button("Export Data", csv, f"{city}-{year}-{month}.pdf", mime='application/octet-stream', key='download-csv')
+
 
     if len(df.index) > 0:
         total_chart = df[["Location Name","Total Reciepts"]].head(25)
